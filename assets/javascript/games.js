4777cc5233c8a339e38a56fc6077cc6fd492e727
Nathan <nathanalston@gmail.com>
2017-12-13 01:07:40 -0500

    Need set array and click to false if game over

--- assets/javascript/games.js
+++ assets/javascript/games.js
@@ -12,49 +12,63 @@ $(document).ready(function() {
   var userTotal = 0;
   var wins = 0;
   var losses = 0;
+  var emeraldcount = 0;
 
   $("#numberWins").text(wins);
   $("#numberLosses").text(losses);
-  $("#pictureToChange").attr("src","assets/images/Sonic.jpg");
+  $("#pictureToChange").attr("src", "assets/images/Sonic.jpg");
+  $("#Emeraldpic").attr("src", "assets/images/chaos_emeralds.jpg");
+  $("#emeraldstrack").text(emeraldcount);
 
-  // $("#reset").on("click", function() {
-    function reset(){
+  $("#reset").on("click", function reset() {
+    // function reset(){
     random = Math.floor(Math.random() * 120 + 19);
     console.log(random);
     $("#randomNumber").text(random);
-    var num1 = Math.floor(Math.random() * 12 + 1);
-    var num2 = Math.floor(Math.random() * 12 + 1);
-    var num3 = Math.floor(Math.random() * 12 + 1);
-    var num4 = Math.floor(Math.random() * 12 + 1);
+    num1 = Math.floor(Math.random() * 12 + 1);
+    num2 = Math.floor(Math.random() * 12 + 1);
+    num3 = Math.floor(Math.random() * 12 + 1);
+    num4 = Math.floor(Math.random() * 12 + 1);
     userTotal = 0;
-//Set that
+    $("#Emeraldpic").attr("src", "assets/images/chaos_emeralds.jpg");
     $("#finalTotal").text(userTotal);
-    // $('#outcome').html('');
-    if (wins >= 8) {
-    $("#pictureToChange").attr("src","assets/images/supersonicemeralds.jpg");
+    $('#outcome').html('');
+    if (emeraldcount === 7) {
+      $("#pictureToChange").attr("src", "assets/images/SuperSonic.jpg");
     } else {
-    $("#pictureToChange").attr("src","assets/images/Sonic.jpg");
+      $("#pictureToChange").attr("src", "assets/images/Sonic.jpg");
     }
-  }
+  });
 
   function winner() {
     // alert("You Won!!");
     wins++;
     $("#numberWins").text(wins);
-    $('#outcome').html('You won!');
-    // if (wins <= 2) {
-    //   $("#Emeraldpic").append($(this).attr("src","assets/images/chaos_emerald.jpg"));
-    // }
-    reset();
+    emeraldcount++;
+    $("#emeraldstrack").text(emeraldcount);
+
+    if (emeraldcount === 7) {
+      $('#outcome').html("You've found all of the Chaos Emeralds. You have now become Super Sonic!");
+      $("#pictureToChange").attr("src", "assets/images/SuperSonic.jpg");
+    } else {
+      $('#outcome').html("Congrations you received a Chaos Emerald!");
+    }
+
   }
 
   function loser() {
     // alert("You Lose!!");
     losses++;
     $("#numberLosses").text(losses);
-    $("#outcome").html('You Lose!');
-    $("#pictureToChange").attr("src","assets/images/robotnik.jpg");
-    reset();
+    $("#pictureToChange").attr("src", "assets/images/robotnik.jpg");
+
+    if (emeraldcount === 0) {
+      $("#outcome").html('You Lose!');
+    } else {
+      emeraldcount--;
+      $("#outcome").html('Oh NO! Dr. Robotnik stole a Chaos Emerald');
+      $("#emeraldstrack").text(emeraldcount);
+    }
   }
 
   $("#image1").on("click", function() {
@@ -69,39 +83,39 @@ $(document).ready(function() {
     }
   })
 
-  $("#image2").on("click", function() {
-    userTotal = userTotal + num2;
-    console.log("New userTotal " + userTotal);
-    $("#finalTotal").text(userTotal);
+$("#image2").on("click", function() {
+  userTotal = userTotal + num2;
+  console.log("New userTotal " + userTotal);
+  $("#finalTotal").text(userTotal);
 
-    if (userTotal == random) {
-      winner()
-    } else if (userTotal > random) {
-      loser()
-    }
-  })
+  if (userTotal == random) {
+    winner()
+  } else if (userTotal > random) {
+    loser()
+  }
+})
 
-  $("#image3").on("click", function() {
-    userTotal = userTotal + num3;
-    console.log("New userTotal " + userTotal);
-    $("#finalTotal").text(userTotal);
+$("#image3").on("click", function() {
+  userTotal = userTotal + num3;
+  console.log("New userTotal " + userTotal);
+  $("#finalTotal").text(userTotal);
 
-    if (userTotal == random) {
-      winner()
-    } else if (userTotal > random) {
-      loser()
-    }
-  })
+  if (userTotal == random) {
+    winner()
+  } else if (userTotal > random) {
+    loser()
+  }
+})
 
-  $("#image4").on("click", function() {
-    userTotal = userTotal + num4;
-    console.log("New userTotal " + userTotal);
-    $("#finalTotal").text(userTotal);
+$("#image4").on("click", function() {
+  userTotal = userTotal + num4;
+  console.log("New userTotal " + userTotal);
+  $("#finalTotal").text(userTotal);
 
-    if (userTotal == random) {
-      winner()
-    } else if (userTotal > random) {
-      loser()
-    }
-  })
+  if (userTotal == random) {
+    winner()
+  } else if (userTotal > random) {
+    loser()
+  }
+})
 });
